28827f2140143caf772605c8e029a6fe4e64557c
James Paterson <james.paterson@theappbusiness.com>
2017-06-07 17:55:39 +0100

    Place snake in initial position

--- game.js
+++ game.js
@@ -1,13 +1,20 @@
 var config = {
-  gridSize: 20,
+  directions: { NORTH: 0, EAST: 1, SOUTH: 2, WEST: 3 },
+  gridSize: 10,
   canvas: {
     height: 300,
     width: 300
   },
   styles: {
     grid: {
+      lineWidth: 1,
       fill: 'grey'
     }
+  },
+  snake: {
+    initialLength: 2,
+    headToken: 'o',
+    tailToken: '~'
   }
 };
 
@@ -16,7 +23,6 @@ var c = canvas.getContext('2d');
 
 canvas.setAttribute('height', config.canvas.height);
 canvas.setAttribute('width', config.canvas.width);
-
 
 function generateGrid() {
   var grid = new Array(config.gridSize);
@@ -31,8 +37,6 @@ function generateGrid() {
 function drawGrid() {
   c.fillStyle = config.styles.grid.fill;
 
-  var gridLineWidth = 1;
-
   var columnWidth = config.canvas.width / config.gridSize;
   var rowHeight = config.canvas.height / config.gridSize;
 
@@ -40,10 +44,63 @@ function drawGrid() {
     var x = i * columnWidth;
     var y = i * rowHeight;
 
-    c.fillRect(x, 0, gridLineWidth, config.canvas.height);
-    c.fillRect(0, y, config.canvas.width, gridLineWidth);
+    c.fillRect(x, 0, config.styles.grid.lineWidth, config.canvas.height);
+    c.fillRect(0, y, config.canvas.width, config.styles.grid.lineWidth);
   }
 }
 
+function generateSnake() {
+  var gridMidPoint = (config.gridSize / 2) - 1;
+
+  return {
+    direction: Object.values(config.directions)[rng(
+      Object.values(config.directions).length
+    )],
+    position: {
+      x: gridMidPoint,
+      y: gridMidPoint
+    },
+    length: config.snake.initialLength
+  }
+}
+
+function plotSnake() {
+  grid[snake.position.x][snake.position.y] = config.snake.headToken;
+
+  switch (snake.direction) {
+    case config.directions.NORTH:
+      grid[snake.position.x][snake.position.y + 1] = config.snake.tailToken;
+      break;
+    case config.directions.EAST:
+      grid[snake.position.x - 1][snake.position.y] = config.snake.tailToken;
+      break;
+    case config.directions.SOUTH:
+      grid[snake.position.x][snake.position.y - 1] = config.snake.tailToken;
+      break;
+    case config.directions.WEST:
+      grid[snake.position.x + 1][snake.position.y] = config.snake.tailToken;
+      break;
+  }
+}
+
+function rng(max) {
+  return Math.floor((Math.random() * max));
+}
+
+function printGrid() {
+  var s = '';
+  for (var y = 0; y <= grid.length - 1; y++) {
+    for (var x = 0; x <= grid[y].length - 1; x++) {
+      s += '|' + (grid[x][y] ? grid[x][y] : ' ');
+    }
+    s += '\n';
+  }
+  console.log(s);
+}
+
 var grid = generateGrid();
-drawGrid(grid);
+var snake = generateSnake();
+
+drawGrid();
+plotSnake();
+printGrid();
