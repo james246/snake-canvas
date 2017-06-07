393b5e9849e9808e59735f3d6509e44fa068c7fe
James Paterson <james.paterson@theappbusiness.com>
2017-06-07 15:24:52 +0100

    Consolidate config variables into one object

--- game.js
+++ game.js
@@ -1,32 +1,49 @@
-var GRID_SIZE = 20;
-
-var CANVAS_HEIGHT = 300;
-var CANVAS_WIDTH = 300;
-
-var styles = {
-  gridFill: 'grey'
+var config = {
+  gridSize: 20,
+  canvas: {
+    height: 300,
+    width: 300
+  },
+  styles: {
+    grid: {
+      fill: 'grey'
+    }
+  }
 };
 
 var canvas = document.getElementById('canvas');
 var c = canvas.getContext('2d');
 
-canvas.setAttribute('height', CANVAS_HEIGHT);
-canvas.setAttribute('width', CANVAS_WIDTH);
+canvas.setAttribute('height', config.canvas.height);
+canvas.setAttribute('width', config.canvas.width);
+
+
+function generateGrid() {
+  var grid = new Array(config.gridSize);
+
+  for (var i = 0; i <= grid.length - 1; i++) {
+    grid[i] = new Array(config.gridSize);
+  }
+
+  return grid;
+}
 
 function drawGrid() {
-  c.fillStyle = styles.gridFill;
+  c.fillStyle = config.styles.grid.fill;
+
   var gridLineWidth = 1;
 
-  var columnWidth = CANVAS_WIDTH / GRID_SIZE;
-  var rowHeight = CANVAS_HEIGHT / GRID_SIZE;
+  var columnWidth = config.canvas.width / config.gridSize;
+  var rowHeight = config.canvas.height / config.gridSize;
 
-  for (var i = 1; i <= GRID_SIZE; i++) {
+  for (var i = 1; i <= config.gridSize; i++) {
     var x = i * columnWidth;
     var y = i * rowHeight;
 
-    c.fillRect(x, 0, gridLineWidth, CANVAS_HEIGHT);
-    c.fillRect(0, y, CANVAS_WIDTH, gridLineWidth);
+    c.fillRect(x, 0, gridLineWidth, config.canvas.height);
+    c.fillRect(0, y, config.canvas.width, gridLineWidth);
   }
 }
 
-drawGrid();
+var grid = generateGrid();
+drawGrid(grid);
