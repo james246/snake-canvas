7ac8cfc9d4bc4bded7537229cd63590312917ac7
James Paterson <james.paterson@theappbusiness.com>
2017-06-07 19:36:50 +0100

    Draw representation of snake to canvas

--- game.js
+++ game.js
@@ -9,6 +9,9 @@ var config = {
     grid: {
       lineWidth: 1,
       fill: 'grey'
+    },
+    snake: {
+      fill: 'red'
     }
   },
   snake: {
@@ -52,7 +55,7 @@ function generateSnake() {
 function plotSnake() {
   grid[snake.position.x][snake.position.y] = config.snake.headToken;
 
-  for (var tailIndex = 1; tailIndex <= config.snake.initialLength - 1; tailIndex++) {
+  for (var tailIndex = 1; tailIndex <= snake.length - 1; tailIndex++) {
     switch (snake.direction) {
       case config.directions.NORTH:
         grid[snake.position.x][snake.position.y + tailIndex] = config.snake.tailToken;
@@ -83,6 +86,21 @@ function drawGrid() {
     c.fillRect(x, 0, config.styles.grid.lineWidth, config.canvas.height);
     c.fillRect(0, y, config.canvas.width, config.styles.grid.lineWidth);
   }
+}
+
+function drawSnake() {
+
+  var cellSize = config.canvas.width / config.gridSize;
+  var headSize = cellSize * 0.8;
+  var padding  = (cellSize - headSize) / 2;
+  
+  var position = (snake.position.x * cellSize) + (headSize / 2) + padding;
+  var radius   = headSize / 2;
+
+  c.beginPath();
+  c.arc(position, position, radius, 0, 2 * Math.PI, false);
+  c.fillStyle = config.styles.snake.fill;
+  c.fill();
 }
 
 // Utility functions
